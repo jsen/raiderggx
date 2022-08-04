d90cb7738f77110059cc2af3a6e1446de5af8b16
jsen <jsenking@gmail.com>
2022-08-04 20:39:26 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Path of Exile :: Raider's Hunt</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
